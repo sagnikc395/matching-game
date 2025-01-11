e33387546e12f9372e8e46c1c77e17293144374b
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-01-11 15:00:37 +0530

    src: added game random and shuffle

--- src/docs/state-management.md
+++ src/docs/state-management.md
@@ -7,3 +7,5 @@
   - lost
 
 - for our use case we are stroing this into a type alias , but can also store into a TS enum.
+
+- we will make a dynamic grid to insert a random unique emoji insider a Set depending on the grid size, and then duplicate them and shuffle them around.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,4 +4,29 @@
     type State = 'start' | 'playing' | 'paused' | 'won' | 'lost'
 
     let state: State = 'start';
+    let size = 20;
+
+    
+    const createGrid = () => {
+        //unique cards 
+        let cards = new Set<string>();
+        //hald because we duplicate the cards 
+        let maxSize = size / 2 ;
+
+        while (cards.size < maxSize) {
+            //pick random emoji 
+            const randomIdx = Math.floor(Math.random() * emoji.length);
+            cards.add(emoji[randomIdx]);
+        }
+
+        //duplicate and shuffle the cards 
+        return shuffle([...cards,...cards]);
+    }
+
+    const shuffle = (array: Items[]) => {
+        return array.sort(() => Math.random() - 0.5);
+    }
+
+    let grid = createGrid();
+
 </script>
